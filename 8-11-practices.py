aaad58b5f2b96228721524716818d4bcb4372718
lvlvlvlvlv <lv_xubo@163.com>
2018-03-18 12:40:55 +0800

    do some practices

--- 8-11-practices.py
+++ 8-11-practices.py
@@ -32,6 +32,7 @@ def AB_merge(a,b,lastA,lastB):
 def __cmp__(x,y):
 
     return cmp(sorted(x),sorted(y))
+
 # 方法二，采用散列表
 def anagramSort(ls):
     dic = dict()
@@ -52,13 +53,90 @@ def anagramSort(ls):
 
     return result
 
+# P 11.3 数组内容旋转后，找到某个值所在的位置
+# 该题最大的特点，就是旋转后的数组，会有两段正常顺序部分（升序），长度不同。为了使用二分法查找，需要确定中间元素属于哪一段。
+# 难点在于当中间元素和两头的元素都相同，无法判断中间元素属于哪一段的时候，就两边都搜素。
+def SearchRotatedList(ls,left,right, item):
+
+    mid = (left + right)/2
+
+    if item == ls[mid]:
+        return mid
+    if right < left:
+        return -1
+
+    if ls[mid] > ls[left]:
+        if item >= ls[left] and item <= ls[mid]:
+            return SearchRotatedList(ls,left,mid-1,item)
+        else:
+            return SearchRotatedList(ls, mid+1, right, item)
+    elif ls[mid] < ls[left]:
+        if item >= ls[mid] and ls <= ls[right]:
+            return SearchRotatedList(ls,mid+1,right,item)
+        else:
+            return SearchRotatedList(ls, left, mid-1, item)
+    elif ls[mid] == ls[left]:
+        result = SearchRotatedList(ls,left,mid-1,item)
+        if result == -1:
+            return SearchRotatedList(ls,mid+1,right,item)
+        else:
+            return result
+
+    return -1
+
+# P11.4 新概念 外部排序
+# 即待排序内容过大，不能全部载入内存，需要分块处理
+# http://blog.csdn.net/jeason29/article/details/50474772
+
+# P11.5
+def SearchwithBlanks(ls,left,right,item):
+
+    if left > right:
+        return -1
+    mid = (left + right)/2
+
+    if ls[mid] == " ":
+        i = mid - 1
+        j = mid + 1
+        while (True):
+            if i < left and j > right:
+                return -1
+            if i >= left and ls[i] != " ":
+                mid = i
+                break
+            elif j <= right and ls[j] != " ":
+                mid = j
+                break
+            else:
+                i -= 1
+                j += 1
+
+    if ls[mid] == item:
+        return mid
+    elif cmp(ls[mid],item) > 0 :
+        return SearchwithBlanks(ls,mid+1,right,item)
+    elif cmp(ls[mid],item) < 0:
+        return SearchwithBlanks(ls, left, mid-1, item)
+
+    return -1
+# P11.6 查找某升序M*N矩阵中的某一元素
+
+def SearchMatrix(m,item):
+    return
+
 if __name__ == "__main__":
 
     #a = [1,7,23,56,109,208,306,0,0,0,0]
     #b = [15,65,222]
     #print AB_merge(a,b,6,2)
 
-    list = ["acre","triangle","race","integral","care","mountain"]
+    #list = ["acre","triangle","race","integral","care","mountain"]
     #list = [1,100,1,20,1,43]
     #print sorted(list,__cmp__)
-    print anagramSort(list)
+
+    #ls = [10,15,20,0,5]
+    #ls = [50,5,20,30,40]
+    #ls = [2,2,2,3,4,2]
+
+    ls = ["amy ","stephen","lily","want"]
+    print SearchwithBlanks(ls,0,len(ls)-1,"gorgerous")
